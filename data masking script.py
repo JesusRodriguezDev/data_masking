83d1d5d9fa2ab56dfb31bb83ad56f9daf34533bb
jesus <jesusrodrigueztech@gmail.com>
2024-10-01 22:18:14 -0600

    created quick script to mask sensitive data in order to create a dummy file while still keeping some of the fields in the test file intact.

---
+++ data masking script.py
@@ -0,0 +1,23 @@
+from faker import Faker
+import pandas as pd
+
+# Initialize the faker
+fake = Faker()
+# Indian locale faker because default faker does not have bank() method.
+fake_in = Faker('en_IN')
+
+# User defined variables.
+input_dir = './input_files/'  # input directory
+output_dir = './output_files/'  # output directory
+data_file = 'your_file.csv'  # input file name
+masked_data_file = 'your_masked_file.csv'  # output masked file name
+
+# Load the data from original file and store it in dataframe.
+df = pd.read_csv(input_dir + data_file)
+
+# Mask fields with sensitive information.
+df['ssn'] = df['ssn'].apply(lambda x: fake.ssn())
+df['bank'] = df['bank'].apply(lambda x: fake_in.bank())
+
+# Save the masked data to a new file.
+df.to_csv(output_dir + masked_data_file, index=False)
